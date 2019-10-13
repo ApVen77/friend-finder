37a689062f66e6f3f2e5771d869eb7ebf62e7713
April Long <april11dance@gmail.com>
2019-10-13 12:04:02 -0600

    working on html and express

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var path = require ("path");
+// Create an instance of the express app.
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
